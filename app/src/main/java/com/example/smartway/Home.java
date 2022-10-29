133faf743c915b537768469f00c300db3fc0cb2a
SAmpathamilaas <90722818+SAmpathamilaas@users.noreply.github.com>
2022-10-29 08:32:38 +0530

    updated

--- app/src/main/java/com/example/smartway/Home.java
+++ app/src/main/java/com/example/smartway/Home.java
@@ -1,23 +1,40 @@
 package com.example.smartway;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.smartway.databinding.ActivityToolBarBinding;
 
 import java.time.Instant;
 
 public class Home extends AppCompatActivity {
-     ImageButton imageButton;
+    ImageButton imageButton;
+
+    TextView toolBarTitle;
+    TextView elecTXTView;
+    ImageView elecIMGView;
+    TextView viewAllCategories;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolBarTitle = findViewById(R.id.toolBarTitle);
+        toolBarTitle.setText("HOME");
 
-        imageButton=findViewById(R.id.imageButton1);
+        imageButton=findViewById(R.id.actionBtn);
+        viewAllCategories = findViewById(R.id.viewAllCategories);
+        elecTXTView = findViewById(R.id.elecTXTView);
+        elecIMGView = findViewById(R.id.elecIMGView);
+
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +43,31 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        elecTXTView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home.this,Services.class);
+                startActivity(intent);
+            }
+        });
+        elecIMGView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home.this,Services.class);
+                startActivity(intent);
+            }
+        });
+
+        viewAllCategories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home.this,CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/smartway/LoginActivity.java
+++ app/src/main/java/com/example/smartway/LoginActivity.java
@@ -2,14 +2,29 @@ package com.example.smartway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView sigup2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sigup2=findViewById(R.id.sigup2);
+
+       sigup2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
